d2defb667cb02677d1e852dcda0e7c57f5019ab1
Jason Garcia <jaygarcia0530@gmail.com>
2023-03-25 14:09:50 -0700

    updated readme file and styled form

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -16,19 +16,14 @@ const ReviewForm = (props) => {
         }
     }
 
-    //Converts 24 hour time to 12 hour time
-    const format = (H, M) => {
-        if (H % 12 == 0) return `12:${(M < 10 ? '0' : '') + M} ${H < 12 ? 'AM' : 'PM'}`;
-        else return `${(H % 12 < 10 ? '0' : '') + H % 12}:${(M < 10 ? '0' : '') + M} ${H < 12 ? 'AM' : 'PM'}`;
-    }
-    // Creating key pair values for object to send to emailjs
+    // Creates key pair values for object to send to emailjs
     arrayToObject(INPUTFIELDS, checklist.input);
-    arrayToObject(RADIOQUESTIONS, checklist.radio);
+    templateParams.textarea = checklist.textarea;
 
     //Sends templateParams values as an object to be used as an email.
     // const sendEmail = (e) => {
     //     e.preventDefault();
-    //     emailjs.send('service_t81sqia', 'template_cb7usop', templateParams, "3wqMebJTzeXwpCO5-")
+    //     emailjs.send('serviceId', 'templateId', templateParams, "")
     //         .then((result) => {
     //             if (result.text === "OK") setSuccess("Sent!");
     //         }, (error) => {
@@ -43,19 +38,15 @@ const ReviewForm = (props) => {
         </div>
     );
 
-    const mappedSafetyQuestions = RADIOQUESTIONS.map((questions, index) =>
-        <ul>
-            <li>{index + 1}. {questions}</li>
-            <li>{checklist.radio[index]}</li>
-        </ul>
-    );
-
     return (
-        <div className="form">
-            <h1>Review Safety Form</h1>
+        <div className="left">
+            <h1>Review Progress Report</h1>
+            <hr/>
             <br />
             {mappedInputFieldNames}
-            {mappedSafetyQuestions}
+            <br/>
+            <p className="font-weight">Goals, Achievements, Comments</p>
+            <p className="white">{checklist.textarea}</p>
             <br />
             {/* <button onClick={(e) => sendEmail(e)}>Send Form</button> */}
             <button onClick={(e) => review(e)}>Edit</button>
--- src/data.js
+++ src/data.js
@@ -2,5 +2,18 @@ export const RADIOQUESTIONS = [
   "Radio question 1"
     ];
 export const INPUTFIELDS = [
-  "Input 1"
+  "Student Name",
+  "DOB",
+  "School",
+  "City",
+  "Grade Level",
+  "Subject",
+  "Parent Name",
+  "Cell",
+  "Hours Completed Today",
+  "Total Hours Completed",
+  "Assessment I",
+  "Presentation",
+  "Practice",
+  "Assessment II/ Application",
 ];
